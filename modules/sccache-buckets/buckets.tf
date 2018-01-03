43452b870c0e9603fe50589f5124f99a106eab82
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-01-03 21:03:22 +0100

    Fixed policy formatting

--- modules/sccache-buckets/buckets.tf
+++ modules/sccache-buckets/buckets.tf
@@ -21,20 +21,20 @@ resource "aws_s3_bucket" "sccache_bucket_us_west_2" {
   }
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicRead",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "*"
-          },
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-west-2/*"
-        }
-      ]
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-west-2/*"
     }
+  ]
+}
 EOF
 }
 
@@ -56,20 +56,20 @@ resource "aws_s3_bucket" "sccache_bucket_us_west_1" {
   }
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicRead",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "*"
-          },
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-west-1/*"
-        }
-      ]
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-west-1/*"
     }
+  ]
+}
 EOF
 }
 
@@ -91,20 +91,20 @@ resource "aws_s3_bucket" "sccache_bucket_us_east_1" {
   }
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicRead",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "*"
-          },
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-east-1/*"
-        }
-      ]
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-east-1/*"
     }
+  ]
+}
 EOF
 }
 
@@ -126,20 +126,20 @@ resource "aws_s3_bucket" "sccache_bucket_us_east_2" {
   }
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicRead",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "*"
-          },
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-east-2/*"
-        }
-      ]
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-us-east-2/*"
     }
+  ]
+}
 EOF
 }
 
@@ -161,19 +161,19 @@ resource "aws_s3_bucket" "sccache_bucket_eu_central_1" {
   }
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicRead",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "*"
-          },
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-eu-central-1/*"
-        }
-      ]
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.prefix}-level-${var.levels[count.index]}-sccache-eu-central-1/*"
     }
+  ]
+}
 EOF
 }
