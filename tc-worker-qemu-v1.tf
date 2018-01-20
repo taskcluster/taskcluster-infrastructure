cb37bba56c143dd76701bb6ef6da90ecd5e2ed7a
Jonas Finnemann Jensen <jopsen@gmail.com>
2018-01-19 16:13:57 -0800

    Updated tc-worker

--- tc-worker-qemu-v1.tf
+++ tc-worker-qemu-v1.tf
@@ -5,6 +5,7 @@ module "tc-worker-qemu-v1" {
   log_port                 = "${var.tc_worker_qemu_log_port}"
   taskcluster_client_id    = "${var.tc_worker_qemu_client_id}"
   taskcluster_access_token = "${var.tc_worker_qemu_access_token}"
+  taskcluster_worker_hash  = "fe8c29966c48dc48e61cf2ef1b47bb373ea729867b93192cfa53bf7b7b043dc6"
   provisioner_id           = "terraform-packet"
   worker_type              = "tc-worker-qemu-v1"
   facility                 = "ams1"
