d8dfc247d0431fa99f28293dace7b54f620b3ba0
Wander Lairson Costa <wander.lairson@gmail.com>
2019-05-03 15:00:29 -0300

    Update the docker-worker secrets script
    This reflects the new password store layout.

--- import-docker-worker-secrets.sh
+++ import-docker-worker-secrets.sh
@@ -3,85 +3,47 @@
 # This script reads docker-worker store in passwordstore and writes
 # up deploy.json.
 #
-# Use: . ./import-docker-worker-secrets
+# Use: deploy/bin/import-docker-worker-secrets
 #
 # Notice you must have taskcluster passwordstore cloned and configured.
 # See ssh://gitolite3@git-internal.mozilla.org/taskcluster/secrets.git
 # for details.
 
-echo 'decrypting..'
-export TF_VAR_docker_worker_private_key=$(pass show docker-worker/shared-env-var-key)
+DEPLOYMENT=taskcluster-net
+base_dir=/tmp/docker-worker-secrets/${DEPLOYMENT}
+rm -rf $base_dir
+mkdir -p $base_dir
+chmod 0700 $base_dir
 
-ssl_certificate=$(pass show tls/star.taskcluster-worker.net | awk '
-  BEGIN {
-    should_print = 0
-  }
-  $0 ~ "BEGIN CERTIFICATE" {
-    should_print = 1
-  }
-  $0 ~ "END CERTIFICATE" {
-    if (should_print) {
-      should_print = 0
-      print $0
-    }
-  }
-  {
-    if (should_print) {
-      print $0
-    }
-  }'
-)
+read -s -p "Enter your gpg passphrase, or enter for none (e.g., Yubikey): " passphrase
+if [ -n "$passphrase" ]; then
+    export PASSWORD_STORE_GPG_OPTS="--passphrase=$passphrase"
+else
+    echo
+    echo '(no passphrase set)'
+fi
 
-intermediate_certificate=$(cat <<EOF
------BEGIN CERTIFICATE-----
-MIIElDCCA3ygAwIBAgIQAf2j627KdciIQ4tyS8+8kTANBgkqhkiG9w0BAQsFADBh
-MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
-d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
-QTAeFw0xMzAzMDgxMjAwMDBaFw0yMzAzMDgxMjAwMDBaME0xCzAJBgNVBAYTAlVT
-MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxJzAlBgNVBAMTHkRpZ2lDZXJ0IFNIQTIg
-U2VjdXJlIFNlcnZlciBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-ANyuWJBNwcQwFZA1W248ghX1LFy949v/cUP6ZCWA1O4Yok3wZtAKc24RmDYXZK83
-nf36QYSvx6+M/hpzTc8zl5CilodTgyu5pnVILR1WN3vaMTIa16yrBvSqXUu3R0bd
-KpPDkC55gIDvEwRqFDu1m5K+wgdlTvza/P96rtxcflUxDOg5B6TXvi/TC2rSsd9f
-/ld0Uzs1gN2ujkSYs58O09rg1/RrKatEp0tYhG2SS4HD2nOLEpdIkARFdRrdNzGX
-kujNVA075ME/OV4uuPNcfhCOhkEAjUVmR7ChZc6gqikJTvOX6+guqw9ypzAO+sf0
-/RR3w6RbKFfCs/mC/bdFWJsCAwEAAaOCAVowggFWMBIGA1UdEwEB/wQIMAYBAf8C
-AQAwDgYDVR0PAQH/BAQDAgGGMDQGCCsGAQUFBwEBBCgwJjAkBggrBgEFBQcwAYYY
-aHR0cDovL29jc3AuZGlnaWNlcnQuY29tMHsGA1UdHwR0MHIwN6A1oDOGMWh0dHA6
-Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RDQS5jcmwwN6A1
-oDOGMWh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RD
-QS5jcmwwPQYDVR0gBDYwNDAyBgRVHSAAMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8v
-d3d3LmRpZ2ljZXJ0LmNvbS9DUFMwHQYDVR0OBBYEFA+AYRyCMWHVLyjnjUY4tCzh
-xtniMB8GA1UdIwQYMBaAFAPeUDVW0Uy7ZvCj4hsbw5eyPdFVMA0GCSqGSIb3DQEB
-CwUAA4IBAQAjPt9L0jFCpbZ+QlwaRMxp0Wi0XUvgBCFsS+JtzLHgl4+mUwnNqipl
-5TlPHoOlblyYoiQm5vuh7ZPHLgLGTUq/sELfeNqzqPlt/yGFUzZgTHbO7Djc1lGA
-8MXW5dRNJ2Srm8c+cftIl7gzbckTB+6WohsYFfZcTEDts8Ls/3HB40f/1LkAtDdC
-2iDJ6m6K7hQGrn2iWZiIqBtvLfTyyRRfJs8sjX7tN8Cp1Tm5gr8ZDOo0rwAhaPit
-c+LJMto4JQtV05od8GiG7S5BNO98pVAdvzr508EIDObtHopYJeS4d60tbvVS3bR0
-j6tJLp07kzQoH3jOlOrHvdPJbRzeXDLz
------END CERTIFICATE-----
-EOF
-)
+# read the secrets into a shell array
+declare -A SECRETS
+set-secret() {
+    echo "Setting secret $1" >&2
+	SECRETS[$1]="${2}"
+}
 
-export TF_VAR_docker_worker_ssl_certificate=$(printf "%s\n%s" "$ssl_certificate" "$intermediate_certificate")
+source <(pass show docker-worker/${DEPLOYMENT}) || exit 1
 
-export TF_VAR_docker_worker_cert_key=$(pass show tls/star.taskcluster-worker.net | awk '
-  BEGIN {
-    should_print = 0
-  }
-  $0 ~ "BEGIN RSA PRIVATE KEY" {
-    should_print = 1
-  }
-  $0 ~ "END RSA PRIVATE KEY" {
-    if (should_print) {
-      should_print = 0
-      print $0
-    }
-  }
-  {
-    if (should_print) {
-      print $0
-    }
-  }
-'
-)
+get-secret() {
+	if [ ${SECRETS[$1]+_} ]; then
+		echo "${SECRETS[$1]}"
+	else
+        echo "Secret $1 not defined" >&2
+        exit 1
+    fi
+}
+
+export TF_VAR_docker_worker_private_key=$(get-secret shared-env-var-key)
+
+export TF_VAR_docker_worker_ssl_certificate=$(get-secret livelog-tls-cert)
+
+export TF_VAR_docker_worker_cert_key=$(get-secret livelog-tls-key)
+
